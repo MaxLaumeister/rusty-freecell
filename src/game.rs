14a3506d10f1c6887cac136b80167a2f5afe1b5b
Maximillian Laumeister <max@maxlaumeister.com>
2024-05-12 17:37:36 -0700

    refactor

--- src/game.rs
+++ src/game.rs
@@ -75,6 +75,166 @@ impl Game {
     pub fn is_won(&self) -> bool {
         self.won
     }
+    
+    pub fn toggle_high_contrast(&mut self) {
+        self.high_contrast = !self.high_contrast;
+    }
+
+    pub fn move_cursor_left(&mut self) {
+        // this modulo trick avoids negative numbers on the unsigned int
+        self.highlighted_card = (self.highlighted_card + FIELD_SIZE - 1) % FIELD_SIZE;
+
+        match self.selected_card_opt {
+            Some(selected_card) => {
+                while !self.move_is_valid(selected_card, self.highlighted_card) && selected_card != self.highlighted_card {
+                    self.move_cursor_left();
+                }
+            }
+            None => {
+                while self.field[self.highlighted_card].last() == None {
+                    self.move_cursor_left();
+                }
+            }
+        }
+    }
+
+    pub fn move_cursor_right(&mut self) {
+        self.highlighted_card = (self.highlighted_card + 1) % FIELD_SIZE;
+
+        match self.selected_card_opt {
+            Some(selected_card) => {
+                while !self.move_is_valid(selected_card, self.highlighted_card) && selected_card != self.highlighted_card {
+                    self.move_cursor_right();
+                }
+            }
+            None => {
+                while self.field[self.highlighted_card].last() == None {
+                    self.move_cursor_right();
+                }
+            }
+        }
+    }
+
+    pub fn quick_stack_to_foundations(&mut self) {
+        let mut made_move = false;
+
+        'outer: for source_column in 0..self.field.len() {
+            for target_column in 0..SUITS {
+                if self.move_is_valid(source_column, target_column) {
+                    self.player_try_execute_move(source_column, target_column);
+                    made_move = true;
+                    break 'outer;
+                }
+            }
+        }
+        // If we made a move, check the new board state for more opportunities
+        if made_move {self.quick_stack_to_foundations()};
+    }
+
+    pub fn handle_card_press(&mut self) {
+        if self.selected_card_opt == None {
+            // Select a card
+            self.selected_card_opt = Some(self.highlighted_card);
+        } else if Some(self.highlighted_card) == self.selected_card_opt {
+            // Deselect a card
+            self.selected_card_opt = None;
+        } else {
+            // Execute a move
+            match self.selected_card_opt {
+                Some(selected_card) => {
+                    self.player_try_execute_move(selected_card, self.highlighted_card);
+                }
+                None => {
+
+                }
+            }
+        }
+    }
+    pub fn player_try_execute_move(&mut self, from: usize, to: usize) {
+        if self.move_is_valid(from, to) {
+            // Execute move, add to undo history
+            self.execute_move(from, to);
+            self.move_count += 1;
+            self.undo_history.push_back(Move{from: from as u8, to: to as u8});
+        }
+    }
+    pub fn perform_undo(&mut self) {
+        let last_move_opt = self.undo_history.pop_back();
+        match last_move_opt {
+            Some(last_move) => {
+                // Perform move in reverse, without checking if it follows the rules
+                self.execute_move(last_move.to as usize, last_move.from as usize);
+                self.move_count -= 1;
+            }
+            None => {
+                // History empty
+            }
+        }
+    }
+
+    fn are_opposite_colors(card1: Card, card2: Card) -> bool {
+        if card1.suit == HEARTS || card1.suit == DIAMONDS {return card2.suit == SPADES || card2.suit == CLUBS};
+        if card1.suit == SPADES || card1.suit == CLUBS {return card2.suit == HEARTS || card2.suit == DIAMONDS};
+        false
+    }
+
+    fn move_is_valid(&self, from: usize, to: usize) -> bool {
+        if from == to {return false;};
+        let from_top_card = self.field[from].last().cloned().unwrap_or_default();
+        let to_top_card = self.field[to].last().cloned().unwrap_or_default();
+        if to < SUITS {
+            // Foundation case
+            if to_top_card.rank != 0 {
+                    return from_top_card.rank == to_top_card.rank + 1 && from_top_card.suit == to_top_card.suit;
+            } else {
+                    return from_top_card.rank == 1 && to as u8 == from_top_card.suit - 1;
+            }
+        } else if to < SUITS + FREE_CELLS {
+            // Free cell case
+            return to_top_card.rank == 0;
+        } else if to < SUITS + FREE_CELLS + TABLEAU_SIZE {
+            // Tableau case
+            if to_top_card.rank != 0 {
+                return from_top_card.rank == to_top_card.rank - 1 && Game::are_opposite_colors(from_top_card, to_top_card);
+            } else {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn execute_move (&mut self, from: usize, to: usize) {
+        // Execute the move
+        // Move "from" card to "to" column
+        let from_card_opt = self.field[from].last();
+        match from_card_opt {
+            Some(&from_card) => {
+                self.field[from].pop();
+                self.field[to].push(from_card);
+            },
+            None => {}
+        }
+        self.selected_card_opt = None;
+        // Check to see if player won or unwon (due to undo)
+        self.won = self.field.iter().all(|stack| stack.len() == RANKS);
+    }
+
+    pub fn print(&self, out: &mut io::Stdout) -> Result<(), io::Error> {
+        if !self.won {
+            self.print_board(out)?;
+            self.print_chrome(out)?;
+        } else {
+            // won
+            out.queue(style::SetAttribute(style::Attribute::Dim))?;
+            self.print_board(out)?;
+            out.queue(style::SetAttribute(style::Attribute::Reset))?;
+            self.print_chrome(out)?;
+            self.print_win(out)?;
+        }
+        out.flush()?;
+        Ok(())
+    }
+
     fn print_board(&self, out: &mut io::Stdout) -> Result<(), io::Error> {
         out.queue(terminal::Clear(terminal::ClearType::All))?;
 
@@ -137,10 +297,6 @@ impl Game {
         }
 
         Ok(())
-    }
-
-    pub fn toggle_high_contrast(&mut self) {
-        self.high_contrast = !self.high_contrast;
     }
 
     fn print_chrome(&self, out: &mut std::io::Stdout) -> Result<(), io::Error> {
@@ -263,163 +419,11 @@ impl Game {
         Ok(())
     }
 
-    pub fn print(&self, out: &mut io::Stdout) -> Result<(), io::Error> {
-        if !self.won {
-            self.print_board(out)?;
-            self.print_chrome(out)?;
-        } else {
-            // won
-            out.queue(style::SetAttribute(style::Attribute::Dim))?;
-            self.print_board(out)?;
-            out.queue(style::SetAttribute(style::Attribute::Reset))?;
-            self.print_chrome(out)?;
-            self.print_win(out)?;
-        }
-        out.flush()?;
-        Ok(())
-    }
-
     fn print_string_at_coord(out: &mut io::Stdout, string: &str, x: u16, y: u16) -> Result<(), io::Error> {
         for (i, line) in string.lines().enumerate() {
             out.queue(cursor::MoveTo(x, y + i as u16))?;
             print!("{}", line);
         }
         Ok(())
-    }
-
-    pub fn move_cursor_left(&mut self) {
-        // this modulo trick avoids negative numbers on the unsigned int
-        self.highlighted_card = (self.highlighted_card + FIELD_SIZE - 1) % FIELD_SIZE;
-
-        match self.selected_card_opt {
-            Some(selected_card) => {
-                while !self.move_is_valid(selected_card, self.highlighted_card) && selected_card != self.highlighted_card {
-                    self.move_cursor_left();
-                }
-            }
-            None => {
-                while self.field[self.highlighted_card].last() == None {
-                    self.move_cursor_left();
-                }
-            }
-        }
-    }
-
-    pub fn move_cursor_right(&mut self) {
-        self.highlighted_card = (self.highlighted_card + 1) % FIELD_SIZE;
-
-        match self.selected_card_opt {
-            Some(selected_card) => {
-                while !self.move_is_valid(selected_card, self.highlighted_card) && selected_card != self.highlighted_card {
-                    self.move_cursor_right();
-                }
-            }
-            None => {
-                while self.field[self.highlighted_card].last() == None {
-                    self.move_cursor_right();
-                }
-            }
-        }
-    }
-
-    pub fn quick_stack_to_foundations(&mut self) {
-        let mut made_move = false;
-
-        'outer: for source_column in 0..self.field.len() {
-            for target_column in 0..SUITS {
-                if self.move_is_valid(source_column, target_column) {
-                    self.player_try_execute_move(source_column, target_column);
-                    made_move = true;
-                    break 'outer;
-                }
-            }
-        }
-        // If we made a move, check the new board state for more opportunities
-        if made_move {self.quick_stack_to_foundations()};
-    }
-
-    pub fn handle_card_press(&mut self) {
-        if self.selected_card_opt == None {
-            // Select a card
-            self.selected_card_opt = Some(self.highlighted_card);
-        } else if Some(self.highlighted_card) == self.selected_card_opt {
-            // Deselect a card
-            self.selected_card_opt = None;
-        } else {
-            // Execute a move
-            match self.selected_card_opt {
-                Some(selected_card) => {
-                    self.player_try_execute_move(selected_card, self.highlighted_card);
-                }
-                None => {
-
-                }
-            }
-        }
-    }
-    fn are_opposite_colors(card1: Card, card2: Card) -> bool {
-        if card1.suit == HEARTS || card1.suit == DIAMONDS {return card2.suit == SPADES || card2.suit == CLUBS};
-        if card1.suit == SPADES || card1.suit == CLUBS {return card2.suit == HEARTS || card2.suit == DIAMONDS};
-        false
-    }
-    fn move_is_valid(&self, from: usize, to: usize) -> bool {
-        if from == to {return false;};
-        let from_top_card = self.field[from].last().cloned().unwrap_or_default();
-        let to_top_card = self.field[to].last().cloned().unwrap_or_default();
-        if to < SUITS {
-            // Foundation case
-            if to_top_card.rank != 0 {
-                    return from_top_card.rank == to_top_card.rank + 1 && from_top_card.suit == to_top_card.suit;
-            } else {
-                    return from_top_card.rank == 1 && to as u8 == from_top_card.suit - 1;
-            }
-        } else if to < SUITS + FREE_CELLS {
-            // Free cell case
-            return to_top_card.rank == 0;
-        } else if to < SUITS + FREE_CELLS + TABLEAU_SIZE {
-            // Tableau case
-            if to_top_card.rank != 0 {
-                return from_top_card.rank == to_top_card.rank - 1 && Game::are_opposite_colors(from_top_card, to_top_card);
-            } else {
-                return true;
-            }
-        }
-        false
-    }
-    fn execute_move (&mut self, from: usize, to: usize) {
-        // Execute the move
-        // Move "from" card to "to" column
-        let from_card_opt = self.field[from].last();
-        match from_card_opt {
-            Some(&from_card) => {
-                self.field[from].pop();
-                self.field[to].push(from_card);
-            },
-            None => {}
-        }
-        self.selected_card_opt = None;
-        // Check to see if player won or unwon (due to undo)
-        self.won = self.field.iter().all(|stack| stack.len() == RANKS);
-    }
-    pub fn player_try_execute_move(&mut self, from: usize, to: usize) {
-        if self.move_is_valid(from, to) {
-            // Execute move, add to undo history
-            self.execute_move(from, to);
-            self.move_count += 1;
-            self.undo_history.push_back(Move{from: from as u8, to: to as u8});
-        }
-    }
-    pub fn perform_undo(&mut self) {
-        let last_move_opt = self.undo_history.pop_back();
-        match last_move_opt {
-            Some(last_move) => {
-                // Perform move in reverse, without checking if it follows the rules
-                self.execute_move(last_move.to as usize, last_move.from as usize);
-                self.move_count -= 1;
-            }
-            None => {
-                // History empty
-            }
-        }
     }
 }
